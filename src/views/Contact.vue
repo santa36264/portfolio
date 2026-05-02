9fbf15de567732c805dc40392a5b499982f4cd7f
santa36264 <semredemssie83@gmail.com>
2026-05-02 15:19:55 +0300

    feat: use EmailJS SDK for contact form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -178,6 +178,7 @@
 
 <script>
 import { ref } from 'vue'
+import emailjs from '@emailjs/browser'
 
 export default {
   name: 'ContactPage',
@@ -199,34 +200,25 @@ export default {
       errorMessage.value = ''
 
       try {
-        const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID
-        const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID
-        const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+        await emailjs.send(
+          import.meta.env.VITE_EMAILJS_SERVICE_ID,
+          import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+          {
+            from_name: formData.value.name,
+            from_email: formData.value.email,
+            subject: formData.value.subject,
+            message: formData.value.message,
+            title: formData.value.subject,
+            name: formData.value.name,
+            email: formData.value.email,
+          },
+          import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+        )
 
-        const response = await fetch('https://api.emailjs.com/api/v1.0/email/send', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            service_id: serviceId,
-            template_id: templateId,
-            user_id: publicKey,
-            template_params: {
-              name: formData.value.name,
-              email: formData.value.email,
-              subject: formData.value.subject,
-              message: formData.value.message,
-              title: formData.value.subject
-            }
-          })
-        })
-
-        if (response.ok) {
-          submitMessage.value = 'Message sent! I\'ll get back to you soon.'
-          formData.value = { name: '', email: '', subject: '', message: '' }
-        } else {
-          throw new Error('Failed to send message.')
-        }
+        submitMessage.value = "Message sent! I'll get back to you soon."
+        formData.value = { name: '', email: '', subject: '', message: '' }
       } catch (err) {
+        console.error('EmailJS error:', err)
         errorMessage.value = 'Something went wrong. Please email me directly at semredemssie36@gmail.com'
       } finally {
         isSubmitting.value = false
